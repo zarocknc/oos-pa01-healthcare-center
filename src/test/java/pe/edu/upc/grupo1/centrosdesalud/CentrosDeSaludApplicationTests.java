31eb9759442cc2dc9704a0158a01046cf558360a
Armandinho <picondev@gmail.com>
2025-11-22 23:08:13 -0300

    fix(test): actualizar package de test a pe.edu.upc.grupo1.centrosdesalud

---
+++ src/test/java/pe/edu/upc/grupo1/centrosdesalud/CentrosDeSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.grupo1.centrosdesalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentrosDeSaludApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
